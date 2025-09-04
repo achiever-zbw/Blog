403259d8048b209a4edf71199b81b53303a0ea33
cjzg666 <1943269811@qq.com>
2025-09-04 13:22:32 +0800

    组件自动导入

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
@@ -16,3 +26,4 @@ export default defineConfig({
     },
   },
 })
+
